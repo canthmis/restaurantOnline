b707848f4e57f1275fc2c7f90c630b2f479e15e2
canthmis <jorge.cruz@thompsonhotels.com>
2022-09-11 09:52:38 -0500

    vendor profile form + validation

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,6 +7,7 @@ from django.contrib.auth.admin import UserAdmin
 class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'username', 'role', 'is_active')
     ordering = ('-date_joined',)
+    #list_display_links = ('username',)
     filter_horizontal = ()
     list_filter = ()
     fieldsets = ()
