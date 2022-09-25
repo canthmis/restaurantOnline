15daf481f26c9dd2ad5e35c79dc9afd3e21314c7
canthmis <jorge.cruz@thompsonhotels.com>
2022-09-25 09:55:49 -0500

    Cambios solicitados por Felipe, regresar y si no funciona reestablecer

--- restaurantOnline_main/static/js/custom.js
+++ restaurantOnline_main/static/js/custom.js
@@ -69,3 +69,34 @@ function onPlaceChanged (){
         }
     }
 }
+
+$(document).ready(function(){
+    // add to cart
+    $('.add_to_cart').on('click', function(e){
+        e.preventDefault();
+        // alert('test123');
+        
+        food_id = $(this).attr('data-id');
+        url = $(this).attr('data-url');
+
+        data = {
+            food_id: food_id,
+        }
+        $.ajax({
+            type: 'GET',
+            url: url,
+            data: data,
+            success: function(response){
+                console.log(response)
+                $('#cart_counter').html(response.cart_counter['cart_counter']);
+                $('#qty-'+food_id).html(response.qty);
+            }
+        })
+    })
+    // place the cart item quantity on load from vendor_detail.html
+    $('.item_qty').each(function(){
+        var the_id = $(this).attr('id')
+        var qty = $(this).attr('data-qty')
+        $('#'+the_id).html(qty)
+    })
+});
